c8879e61bbfa50cf4a9a1f8fa8b91a54e905e81e
Adrián Rey <adrianreylomba@gmail.com>
2022-06-12 11:00:53 +0200

    Set appropriate status code when validating fields

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,5 +1,11 @@
+const { ValidationError } = require("joi");
+
 const handleError = (error, req, res, next) => {
   console.error(error);
+
+  if (error instanceof ValidationError) {
+    error.statusCode = 400;
+  }
 
   res.statusCode = error.statusCode || 500;
   res.send({ status: "error", message: error.message });
